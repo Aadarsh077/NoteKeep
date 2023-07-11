916c70e58305b53cfa688116d6dcef0082b2f130
Aadarsh Krishnan <aadarshk777@gmail.com>
2023-07-11 13:42:18 +0530

    added all minor changes

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
