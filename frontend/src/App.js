8e876f0479280f89f703956167e2b616ff60eb40
Aadarsh Krishnan <aadarshk777@gmail.com>
2023-07-12 18:31:19 +0530

    fetched notes from context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -6,6 +7,10 @@ function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/about" element={<About />}/>
+      </Routes>
     </>
   );
 }
