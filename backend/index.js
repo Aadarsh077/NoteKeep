7db92bc8a903054cabccadbaab1432da28db18c9
Aadarsh Krishnan <aadarshk777@gmail.com>
2024-06-18 12:51:37 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,18 @@
 const connectToMongo = require('./db');
-const express = require('express')
+const express = require('express');
 
 connectToMongo();
-const app = express()
-const port = 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-var cors = require('cors')
-app.use(cors())
-app.use(express.json())
+var cors = require('cors');
+app.use(cors());
+app.use(express.json());
 
+// Available Routes
+app.use('/api/auth', require('./routes/authen'));
+app.use('/api/notes', require('./routes/notes'));
 
-//Available Routes
-app.use('/api/auth', require('./routes/authen'))
-app.use('/api/notes', require('./routes/notes'))
-
-app.listen(port, () => {
-  console.log(`iNotebook backend listening at http://localhost:${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`KeepNote backend listening at http://localhost:${PORT}`);
+});
